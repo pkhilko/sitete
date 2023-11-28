d4d188254add1ccf8b9e2847b8ebe16255c80b7b
mac512 <mac512@mac512s-MacBook-Pro.local>
2023-11-28 23:16:58 +0200

    add acon grid

---
+++ src/components/IconsCollage.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import GitHub from '$lib/assets/icons/github.svg';
+	import Docker from '$lib/assets/icons/docker.svg';
+	import Google from '$lib/assets/icons/google.svg';
+	import Facebook from '$lib/assets/icons/facebook.svg';
+	import React from '$lib/assets/icons/react.svg';
+	import Twitter from '$lib/assets/icons/twitter.svg';
+	import Vue from '$lib/assets/icons/vue.svg';
+	import Python from '$lib/assets/icons/python.svg';
+	import Dropbox from '$lib/assets/icons/dropbox.svg';
+</script>
+
+<div class="outerdiv">
+	<img class="innerdiv" src={GitHub} alt="GitHub" />
+	<img class="innerdiv" src={Docker} alt="Docker" />
+	<img class="innerdiv" src={Google} alt="Google" />
+	<img class="innerdiv" src={Facebook} alt="Facebook" />
+	<img class="innerdiv" src={React} alt="React" />
+	<img class="innerdiv" src={Twitter} alt="Google" />
+	<img class="innerdiv" src={Vue} alt="GitHub" />
+	<img class="innerdiv" src={Dropbox} alt="Dropbox" />
+	<img class="innerdiv" src={Python} alt="Python" />
+</div>
+
+<style>
+	.outerdiv {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.innerdiv {
+		flex-grow: 1;
+		width: 33%;
+		height: 33%;
+	}
+</style>
--- src/components/Showcase.svelte
+++ src/components/Showcase.svelte
@@ -4,6 +4,7 @@
 	const computedClassName = imgPosition === 'left' ? 'grid-container img-left' : 'grid-container';
 	export let title: string;
 	export let content: string;
+	import IconsCollage from '../components/IconsCollage.svelte';
 </script>
 
 <div class={computedClassName}>
@@ -13,11 +14,7 @@
 		<Button />
 	</div>
 	<div class="two">
-		<img
-			class="img"
-			src="https://www.imgacademy.com/sites/default/files/school-about-us-slider-22.jpg"
-			alt="blev"
-		/>
+		<slot />
 	</div>
 </div>
 
@@ -47,12 +44,5 @@
 		grid-area: b;
 		border: 2px solid #c91818;
 		padding: 24px;
-	}
-	.img {
-		display: block;
-		margin-left: auto;
-		margin-right: auto;
-		width: 100%;
-		object-fit: fill;
 	}
 </style>
